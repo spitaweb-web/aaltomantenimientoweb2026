7d337e3bd000bc8a7bcd120d692adee7db610b75
spitaweb-web <spitaweb@gmail.com>
2026-04-27 13:15:12 -0300

    Use final AALTO landing with client logos

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './AppFixed.tsx';
+import App from './AppFinal.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
