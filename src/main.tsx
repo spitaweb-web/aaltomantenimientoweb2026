3bfa60454e82472e695bb431be976050d2685859
spitaweb-web <spitaweb@gmail.com>
2026-05-09 13:21:39 -0300

    Activar email corporativo AALTO

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './aalto-final.css'
 import './whatsapp-widget.ts'
+import './contact-email-fix.ts'
 import App from './AppFixed.tsx'
 
 createRoot(document.getElementById('root')!).render(
