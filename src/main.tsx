9f2e0b8e82a35ff7089dd0af2cf406880540cef8
spitaweb-web <spitaweb@gmail.com>
2026-05-20 15:41:08 -0300

    Revert render entry to AppFixed

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ import './aalto-homepage-literal-fix.css'
 import './aalto-client-logos-final.css'
 import './aalto-final-hard-fixes.css'
 import './aalto-mobile-final-fix.css'
-import App from './App.tsx'
+import App from './AppFixed.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
